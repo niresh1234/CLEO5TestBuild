aace42938514a515f951fd9d3e53374c0b6963c3
MiranDMC <Miran4@op.pl>
2024-03-01 17:30:26 +0100

    0AA6 pointer validation relaxed for legacy scripts (#82)
    * 0AA6 pointer validation relaxed for legacy scripts
    
    * fixup! 0AA6 pointer validation relaxed for legacy scripts

--- cleo_plugins/MemoryOperations/MemoryOperations.cpp
+++ cleo_plugins/MemoryOperations/MemoryOperations.cpp
@@ -419,7 +419,17 @@ public:
     static OpcodeResult __stdcall opcode_0AA6(CLEO::CRunningScript* thread)
     {
         auto func = OPCODE_READ_PARAM_PTR();
-        auto obj = OPCODE_READ_PARAM_PTR();
+
+        void* obj = nullptr;
+        if (!IsLegacyScript(thread))
+        {
+            obj = OPCODE_READ_PARAM_PTR();
+        }
+        else
+        {
+            obj = (void*)OPCODE_READ_PARAM_INT(); // at least one mod used 0AA6 with 0 as struct argument (effectively turning it into 0AA5 opcode...)
+        }
+
         auto numArgs = OPCODE_READ_PARAM_INT();
         auto numPop = OPCODE_READ_PARAM_INT();
 
