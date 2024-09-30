397714806f26b48d2275e953ed13904901b36b92
MiranDMC <Miran4@op.pl>
2024-09-30 21:22:47 +0200

    Unit tests log style updates. (#219)
    * Unit tests log style updates.
    
    * fixup! Unit tests log style updates.

--- tests/cleo_tests/.Compile_All.bat
+++ tests/cleo_tests/.Compile_All.bat
@@ -7,18 +7,23 @@ SETLOCAL EnableDelayedExpansion
 @REM Delete all .s files in the current directory and subdirectories
 for /f "delims=" %%i in ('dir /b /s *.s') do (
     set p=%%i 
-    echo Deleting !p:%__CD__%=!...
+    echo Deleting !p:%__CD__%=!
     del "%%i"
 )
+echo.
 
 @REM Compile all .txt files in the current directory and subdirectories
 for /f "delims=" %%i in ('dir /b /s *.txt') do (
     if not "%%~nxi" == "cleo_tester.txt" (
         set p=%%i 
-        echo Compiling !p:%__CD__%=!...
+        echo Compiling !p:%__CD__%=!
         %SANNY% --compile "%%i" "%%~dpni.s" --no-splash --mode sa_sbl    
+        if not exist "%%~dpni.s" (
+            echo ERROR: Failed to build !p:%__CD__%=!
+        )        
     )
 )
+echo.
 
 echo Done.
 pause
